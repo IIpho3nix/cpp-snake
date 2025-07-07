346e1d8cd1927bf59c4924308a5ac223836a79dd
IIpho3nix <79083095+IIpho3nix@users.noreply.github.com>
2025-07-08 01:06:27 +0300

    optimize build flags

--- build.bat
+++ build.bat
@@ -9,21 +9,21 @@ if exist "%raylib_path%\lib\libraylib.a" (
     echo libraylib.a found in lib folder.
 ) else (
     echo ERROR: libraylib.a not found in lib folder.
-	exit /b 1
+    exit /b 1
 )
 
 if exist "%raylib_path%\lib\libraylibdll.a" (
     echo libraylibdll.a found in lib folder.
 ) else (
     echo ERROR: libraylibdll.a not found in lib folder.
-	exit /b 1
+    exit /b 1
 )
 
 if exist "%raylib_path%\lib\raylib.dll" (
     echo raylib.dll found in lib folder.
 ) else (
     echo ERROR: raylib.dll not found in lib folder.
-	exit /b 1
+    exit /b 1
 )
 
 :: Check if the include folder contains raylib.h, raymath.h, and rlgl.h
@@ -32,33 +32,33 @@ if exist "%raylib_path%\include\raylib.h" (
     echo raylib.h found in include folder.
 ) else (
     echo ERROR: raylib.h not found in include folder.
-	exit /b 1
+    exit /b 1
 )
 
 if exist "%raylib_path%\include\raymath.h" (
     echo raymath.h found in include folder.
 ) else (
     echo ERROR: raymath.h not found in include folder.
-	exit /b 1
+    exit /b 1
 )
 
 if exist "%raylib_path%\include\rlgl.h" (
     echo rlgl.h found in include folder.
 ) else (
     echo ERROR: rlgl.h not found in include folder.
-	exit /b 1
+    exit /b 1
 )
 
 :: Finish
 echo Raylib successfully installed.
 
 echo Creating build if it doesn't exist...
-if not exist build mkdir build	
+if not exist build mkdir build    
 
 echo Compiling all .cpp and .c files in src folder...
 for %%f in (src\*.cpp src\*.c %custom_src%) do (
     echo Compiling %%~nxf...
-    g++ -O3 -I"%raylib_path%\include" -I"src" -I"src/include" %custom_include% -c %%f -o build\%%~nf.o
+    g++ -O3 -flto -fno-exceptions -s -ffunction-sections -fdata-sections -I"%raylib_path%\include" -I"src" -I"src/include" %custom_include% -c %%f -o build\%%~nf.o
 )
 
 if exist build\raylib.dll (
@@ -67,12 +67,12 @@ if exist build\raylib.dll (
 
 echo Creating executable...
 if "%link%"=="dynamic" (
-	echo Dynamic linking raylib
-	g++ -O3 -mwindows -L"%raylib_path%\lib" -L"src/lib" build\*.o -o build\%name%.exe -l:raylib.dll -lgdi32 -lwinmm
-	copy raylib\lib\raylib.dll build\raylib.dll
+    echo Dynamic linking raylib
+    g++ -O3 -flto -fno-exceptions -s -Wl,--gc-sections -mwindows -L"%raylib_path%\lib" -L"src/lib" build\*.o -o build\%name%.exe -l:raylib.dll -lgdi32 -lwinmm
+    copy raylib\lib\raylib.dll build\raylib.dll
 ) else (
-	echo Static linking raylib
-	g++ -O3 -mwindows -L"%raylib_path%\lib" -L"src/lib" build\*.o -o build\%name%.exe -lraylib -lgdi32 -lwinmm
+    echo Static linking raylib
+    g++ -O3 -flto -fno-exceptions -s -Wl,--gc-sections -mwindows -L"%raylib_path%\lib" -L"src/lib" build\*.o -o build\%name%.exe -lraylib -lgdi32 -lwinmm
 )
 
 echo Build completed.
